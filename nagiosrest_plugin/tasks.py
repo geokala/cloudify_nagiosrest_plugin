0d53536f7275a852965340f55109e074460b1a14
Madda <nobody@nowhere.local>
2018-05-31 11:50:05 +0100

    Basic setup

---
+++ nagiosrest_plugin/tasks.py
@@ -0,0 +1,42 @@
+import requests
+# TODO: Add requests to requirements
+
+from cloudify import ctx as cloudify_ctx
+from cloudify.decorators import operation
+
+
+def _get_instance_id_url(address, instance_id):
+    # TODO: HTTPS
+    return 'http://{address}/nagiosrest/{instance_id}'.format(
+        address=address,
+        instance_id=instance_id,
+    )
+
+
+@operation
+def add_monitoring(ctx,
+                   nagiosrest_address, nagiosrest_user, nagiosrest_pass,
+                   target_type, heal_on_failure,
+                   instance_ip_property):
+    # TODO: Do some error handling
+    requests.put(
+        _get_instance_id_url(nagiosrest_address, ctx.instance.id),
+        auth=(nagiosrest_user, nagiosrest_pass),
+        json={
+            'instance_ip': ctx.instance[instance_ip_property],
+            'tenant': cloudify_ctx.tenant_name,
+            'deployment': ctx.deployment.id,
+            'target_type': target_type,
+            'heal_on_failure': heal_on_failure,
+        },
+    )
+
+
+@operation
+def remove_monitoring(ctx, nagiosrest_address, nagiosrest_user,
+                      nagiosrest_pass):
+    # TODO: Do some error handling
+    requests.delete(
+        _get_instance_id_url(nagiosrest_address, ctx.instance.id),
+        auth=(nagiosrest_user, nagiosrest_pass),
+    )
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cloudify-nagiosrest-plugin',
+    version='0.1.1',
+    packages=[
+        'nagiosrest_plugin',
+    ],
+    install_requires=['cloudify-plugins-common>=3.3.1'],
+)
